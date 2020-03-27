cd28faf8a9ea21a1f7ae0e95915c394f5c184ced
Felimas <felipe.limas@hotmail.com>
2020-03-26 22:08:16 -0300

    Meu primeiro Commit

---
+++ works/aulajs/index.js
@@ -0,0 +1,8 @@
+// Criar um programa que calcula a média
+// das notas entre os aulos e envia
+// mensagem do calculo da média.
+
+const aluno01     = "Felipe"
+const aluno02     = 'José'
+const notaAluno01 = 9.8
+const notaAluno02 = 10
